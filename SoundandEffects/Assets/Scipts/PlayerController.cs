9e0c9c082c6157c361230eaa6c2820885b940b99
Maxwell Hernandez <hernandezm3@roverkids.org>
2024-05-08 13:27:00 -0400

    Completed 3.1 Step 5

--- SoundandEffects/Assets/Scipts/PlayerController.cs
+++ SoundandEffects/Assets/Scipts/PlayerController.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    public float jumpForce = 10;
+    public float gravityModifier;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier; 
+        
         
 
     }
@@ -18,7 +22,7 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space)) 
         {
-            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
 
         }
 
