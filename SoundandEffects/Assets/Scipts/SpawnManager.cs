aedb04c9c2974df0f322cccfbe4d4947d26b273d
Maxwell Hernandez <hernandezm3@roverkids.org>
2024-05-08 13:44:29 -0400

    Completed 3.1 Step 9

--- SoundandEffects/Assets/Scipts/SpawnManager.cs
+++ SoundandEffects/Assets/Scipts/SpawnManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,17 +7,27 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
-    }
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+     }
 
     // Update is called once per frame
     void Update()
     {
         
     }
+
+    void SpawnObstacle() 
+    {
+
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
 }
+
+
