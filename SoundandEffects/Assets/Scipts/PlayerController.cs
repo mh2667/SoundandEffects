23fa92a61245a2db6bc8a824314d0d406d76d3b9
Maxwell Hernandez <hernandezm3@roverkids.org>
2024-05-17 13:09:05 -0400

    Completed 3.4 Step 2

--- SoundandEffects/Assets/Scipts/PlayerController.cs
+++ SoundandEffects/Assets/Scipts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     private Animator playerAnim;
+    public ParticleSystem explosionParticle;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
@@ -50,6 +51,7 @@ public class PlayerController : MonoBehaviour
             gameOver = true;
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
         }
             
 
